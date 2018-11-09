310c45eb2dace0b7acc994e99a6c82754fb58095
Víctor Cruz <vcruzgon@itam.mx>
2018-11-09 06:56:22 -0600

    Cambio menor

--- runDocker.sh
+++ runDocker.sh
@@ -4,6 +4,6 @@
 docker rm -f skynetdevelop/mercamita_dev
 
 docker run -it --name mercamita_dev -p 1337:1337 -p 27017:27017 \
-    -v 'pwd'/.:/mercamita/. -d skynetdevelop/mercamita_dev
+    -v `pwd`/:/mercamita/ -d skynetdevelop/mercamita_dev
 
 docker exec -d mercamita_dev service mongodb starts
