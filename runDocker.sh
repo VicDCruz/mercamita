12a4bff0a63f68a4e0d19cc11aa368d7b1b403ec
Víctor Cruz <vcruzgon@itam.mx>
2018-11-09 07:06:15 -0600

    Poner a correr mongo e instalar nuevas dependencias

--- runDocker.sh
+++ runDocker.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Remove last running docker container
-docker rm -f skynetdevelop/mercamita_dev
+docker rm -f mercamita_dev
 
 docker run -it --name mercamita_dev -p 1337:1337 -p 27017:27017 \
-    -v `pwd`/:/mercamita/ -d skynetdevelop/mercamita_dev
+    -w /mercamita -v `pwd`/:/mercamita/ -d skynetdevelop/mercamita_dev
 
-docker exec -d mercamita_dev service mongodb starts
+docker exec -d mercamita_dev service mongodb start
+docker exec -d mercamita_dev npm i
