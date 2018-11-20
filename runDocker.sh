5400782882956a509f40bdc056e20ffd347c88da
Víctor Cruz <vcruzgon@itam.mx>
2018-11-20 00:20:07 -0600

    Cambios en la barra de estados

--- runDocker.sh
+++ runDocker.sh
@@ -6,5 +6,5 @@ docker rm -f mercamita_dev
 docker run -it --name mercamita_dev -p 1337:1337 -p 27017:27017 \
     -w /mercamita -v `pwd`/:/mercamita/ -d skynetdevelop/mercamita_dev
 
-docker exec -d mercamita_dev service mongodb start
-docker exec -d mercamita_dev npm i
+docker exec mercamita_dev service mongodb start
+docker exec -d mercamita_dev npm install
