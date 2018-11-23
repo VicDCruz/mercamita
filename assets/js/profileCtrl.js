928d4d454c3f1798ed13845b290783ebe3c53fbd
MoniiAlba <soeur_azmaria@hotmail.com>
2018-11-22 22:43:13 -0600

    funciona con dialog viejo

--- assets/js/profileCtrl.js
+++ assets/js/profileCtrl.js
@@ -1,13 +1,48 @@
 app.controller('profileCtrl', ($scope, toastr, $http, $window) => {
-    $scope.user = null;
-    $scope.init = () => {
-        if (!$scope.user) $window.location.href = '/';
-    };
-    $scope.logout = () => {
-        $http.get('/logout')
-            .then((response) => {
-                toastr.success('Sesión cerrada', 'Éxito');
-                $window.location.href = '/';
-            });
-    };
-})
+      $scope.user = null;
+      $scope.init = () => {
+      if (!$scope.user) $window.location.href = '/';
+   };
+   $scope.logout = () => {
+      $http.get('/logout')
+      .then((response) => {
+      toastr.success('Sesión cerrada', 'Éxito');
+      $window.location.href = '/';
+      });
+   };
+   $scope.getProds = (id) =>{ 
+      $http.get('/product?id=' + id).then(function(result) {
+         var res = result.data[0]
+         console.log(res)
+         $scope.prods.push(res)
+      }).catch(function() {
+         return null
+      });
+   };
+   $scope.getWL = (id) =>{
+      $http.get('/product?id=' + id).then(function(result) {
+         var res = result.data[0]
+         console.log(res)
+         $scope.prodsWL.push(res)
+      }).catch(function() {
+         return null
+      });
+   };
+   $scope.getProductos = () =>{ 
+      var prod = $scope.user.products
+      var prodWL = $scope.user.wishList
+      console.log($scope.user)
+      if(prod != undefined)
+         prod.forEach(element => {
+         $scope.getProds(element)
+      });
+      
+      if(prodWL != undefined)
+         prodWL.forEach(element =>{
+         $scope.getWL(element)
+      })
+      };
+      $scope.cambiaProdModal = (p) =>{ 
+      $scope.prodModal = p
+      };
+   });
