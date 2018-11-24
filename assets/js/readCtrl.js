dc5e6f9ac2bf94caf39650c97991468c3d3d7379
Víctor Cruz <vcruzgon@itam.mx>
2018-11-24 12:02:59 -0600

    Inserción de ngDialog

--- assets/js/readCtrl.js
+++ assets/js/readCtrl.js
@@ -1,9 +1,17 @@
-app.controller('readCtrl', ($scope, toastr, $http, $window) => {
+app.controller('readCtrl', ($scope, toastr, $http, ngDialog, $window) => {
     $scope.logout = () => {
         $http.get('/logout')
             .then((response) => {
                 toastr.success('Sesión cerrada', 'Éxito');
                 $window.location.href = '/';
             });
+    };
+    $scope.clickToOpen = function () {
+        console.log('Hola');
+        
+        ngDialog.open({
+            template: '<p>my template</p>',
+            plain: true
+        });
     };
 })
