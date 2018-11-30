44b14a03336d099eee884e18788188b70ce0e730
Víctor Cruz <vcruzgon@itam.mx>
2018-11-30 13:03:14 -0600

    Configuración inicial del correo

---
+++ config/email.js
@@ -0,0 +1,21 @@
+/**
+ * HTTP Server Settings
+ * (sails.config.http)
+ *
+ * Configuration for the underlying HTTP server in Sails.
+ * (for additional recommended settings, see `config/env/production.js`)
+ *
+ * For more information on configuration, check out:
+ * https://sailsjs.com/config/http
+ */
+
+module.exports.email = {
+    service: 'Gmail',
+    auth: {
+        user: 'do.not.reply.skynet@gmail.com', 
+        pass: 'gh637zsrjpk7rcqywscmav'
+    },
+    templateDir: 'views/email/verifyTemplate',
+    testMode: false,
+    from: 'do.not.reply.skynet@gmail.com'
+}
